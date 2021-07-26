3fb51590ed4481b8dbe48fc664adfd3098a0b2f6
vishal R <vishal206ani@gmail.com>
2021-07-26 23:56:40 +0530

    splash screen linked&updated

--- app/src/main/java/com/example/couponbazar/Splash_screen.java
+++ app/src/main/java/com/example/couponbazar/Splash_screen.java
@@ -2,13 +2,50 @@ package com.example.couponbazar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Splash_screen extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN_TIME_OUT=1000;
+    private FirebaseAuth mAuth;
+    FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //below statement is not show full screen but its not good looking...check it if u want
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash_screen);
+
+        mAuth=FirebaseAuth.getInstance();
+
+        if(mAuth!=null){
+            currentUser= mAuth.getCurrentUser();
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                if(currentUser==null){
+                    startActivity(new Intent(Splash_screen.this,SignupActivity.class));
+                    finish();
+                }else{
+                    Intent i=new Intent(Splash_screen.this,HomeActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+
+            }
+        },SPLASH_SCREEN_TIME_OUT);
+
     }
 }
